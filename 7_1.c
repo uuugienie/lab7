680d2ebdae956eebec29ab93bbcbc6fee32cc01d
6504062610331 Usanee <s6504062610331@email.kmutnb.ac.th>
2026-02-17 13:03:55 +0700

    Lab7no1
    Commit 1>> Function Demo1:Function Calling

---
+++ 7_1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+double functionM(void);
+int functionN(int n, double x);
+double functionO(double, int, double, int);
+double functionP(int a, int n, int b, int c);
+
+int main(void)
+{
+    double a;         
+    int b, c, d, e;
+    double r, s, t, u, v;
+
+    a = functionM();   
+    printf("%lf\n", a);
+
+    return 0;
+}
+
